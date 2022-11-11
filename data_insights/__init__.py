93988585049a56ae80158c32b2a374dffa0c67b0
Patricio Cerda Mardini <pcerdam@live.com>
2022-11-11 20:41:04 +0100

    add __name__ and __version__

---
+++ data_insights/__init__.py
@@ -0,0 +1,14 @@
+from data_insights import base
+from data_insights import cleaners
+from data_insights import helpers
+from data_insights import imputers
+from data_insights import insights
+from data_insights import splitters
+
+
+__version__ = '0.0.1'
+__name__ = "data_insights"
+
+
+__all__ = ['base', 'cleaners', 'helpers', 'imputers', 'insights', 'splitters',
+           '__version__', '__name__']
