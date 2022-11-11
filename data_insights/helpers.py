92b8cb7a4151de24d1e9a2a97837bda5e7493fad
Patricio Cerda Mardini <pcerdam@live.com>
2022-11-11 18:12:31 +0100

    add functional cleaner + tests

---
+++ data_insights/helpers.py
@@ -0,0 +1,47 @@
+import os
+import random
+import logging
+from typing import Iterable
+
+import colorlog
+import numpy as np
+import pandas as pd
+
+from type_infer.helpers import is_nan_numeric
+
+
+def initialize_log():
+    pid = os.getpid()
+
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(colorlog.ColoredFormatter())
+
+    logging.basicConfig(handlers=[handler])
+    log = logging.getLogger(f'type_infer-{pid}')
+    log_level = os.environ.get('TYPE_INFER_LOG', 'DEBUG')
+    log.setLevel(log_level)
+    return log
+
+
+log = initialize_log()
+
+
+def seed(seed_nr: int) -> None:
+    np.random.seed(seed_nr)
+    random.seed(seed_nr)
+
+
+def filter_nan_and_none(series: Iterable) -> list:
+    return [x for x in series if not is_nan_numeric(x) and x is not None]
+
+
+def get_ts_groups(df: pd.DataFrame, tss) -> list:
+    ##############################
+    # TODO - Should refactor this
+    ##############################
+    group_combinations = ['__default']
+    if tss.group_by:
+        groups = [tuple([g]) if not isinstance(g, tuple) else g
+                  for g in list(df.groupby(by=tss.group_by).groups.keys())]
+        group_combinations.extend(groups)
+    return group_combinations
---
+++ tests/integration_tests/test_data_insights.py
@@ -0,0 +1,104 @@
+import unittest
+import numpy as np
+import pandas as pd
+
+from type_infer.infer import infer_types
+
+from data_insights.cleaners import cleaner
+from data_insights.imputers import NumericalImputer, CategoricalImputer
+
+
+class TestTypeInference(unittest.TestCase):
+    def test_0_airline_sentiment(self):
+        df = pd.read_csv("tests/data/airline_sentiment_sample.csv")
+        inferred_types = infer_types(df, pct_invalid=0)
+        target = 'airline_sentiment'
+        tss = {
+            'is_timeseries': False,
+        }
+        cdf = cleaner(data=df,
+                      dtype_dict=inferred_types.dtypes,
+                      pct_invalid=0.01,
+                      identifiers={},
+                      target=target,
+                      mode='train',
+                      timeseries_settings=tss,
+                      anomaly_detection=False,
+                      imputers={},
+                      custom_cleaning_functions={})
+
+        self.assertTrue(isinstance(cdf, pd.DataFrame))  # TODO: better asserts here
+
+    def test_1_hdi(self):
+        df = pd.read_csv("tests/data/hdi.csv")
+        inferred_types = infer_types(df, pct_invalid=0)
+        target = 'Development Index'
+        tss = {
+            'is_timeseries': False,
+        }
+        cdf = cleaner(data=df,
+                      dtype_dict=inferred_types.dtypes,
+                      pct_invalid=0.01,
+                      identifiers={},
+                      target=target,
+                      mode='train',
+                      timeseries_settings=tss,
+                      anomaly_detection=False,
+                      imputers={},
+                      custom_cleaning_functions={})
+        self.assertTrue(isinstance(cdf, pd.DataFrame))
+
+    def test_2_imputers(self):
+        df = pd.read_csv("tests/data/hdi.csv")
+        df = df.rename(columns={'GDP ($ per capita)': 'GDP', 'Area (sq. mi.)': 'Area', 'Literacy (%)': 'Literacy'})
+        df['Infant mortality '] = df['Infant mortality '].apply(lambda x: 'High' if x >= 20 else 'Low')
+
+        inferred_types = infer_types(df, pct_invalid=0)
+        target = 'Development Index'
+        tss = {
+            'is_timeseries': False,
+        }
+
+        cat_mode_impute_col = 'Infant mortality '
+        num_mean_impute_col = 'Population'
+        num_mode_impute_col = 'GDP'
+        num_zero_impute_col = 'Pop. Density '
+        num_median_impute_col = 'Area'
+
+        cols = [num_mean_impute_col, num_mode_impute_col, num_zero_impute_col, num_median_impute_col,
+                cat_mode_impute_col]
+
+        for col in cols:
+            df[col].iloc[0] = np.nan  # replace first row values with nans
+
+        imputers = {
+            num_mean_impute_col: NumericalImputer(value='mean', target=num_mean_impute_col),
+            num_mode_impute_col: NumericalImputer(value='mode', target=num_mode_impute_col),
+            num_median_impute_col: NumericalImputer(value='median', target=num_median_impute_col),
+            num_zero_impute_col: NumericalImputer(value='zero', target=num_zero_impute_col),
+            cat_mode_impute_col: CategoricalImputer(value='mode', target=cat_mode_impute_col)
+
+        }
+
+        num_mean_target_value = df[num_mean_impute_col].iloc[1:].mean()
+        num_mode_target_value = df[num_mode_impute_col].iloc[1:].mode().iloc[0]
+        num_median_target_value = df[num_median_impute_col].iloc[1:].median()
+        num_zero_target_value = 0.0
+        cat_mode_target_value = df[cat_mode_impute_col].iloc[1:].mode().iloc[0]
+
+        cdf = cleaner(data=df,
+                      dtype_dict=inferred_types.dtypes,
+                      pct_invalid=0.01,
+                      identifiers={},
+                      target=target,
+                      mode='train',
+                      timeseries_settings=tss,
+                      anomaly_detection=False,
+                      imputers=imputers,
+                      custom_cleaning_functions={})
+
+        assert cdf[num_mean_impute_col].iloc[0] == num_mean_target_value
+        assert cdf[num_mode_impute_col].iloc[0] == num_mode_target_value
+        assert cdf[num_zero_impute_col].iloc[0] == num_zero_target_value
+        assert cdf[num_median_impute_col].iloc[0] == num_median_target_value
+        assert cdf[cat_mode_impute_col].iloc[0] == cat_mode_target_value
