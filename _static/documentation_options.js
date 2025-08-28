d7712ae5ef5c9d92880b37faf4044ae0864f2fc5
ZoranPandovski <zoran.pandovski@gmail.com>
2025-08-28 13:49:38 +0000

    Rebuilt the docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '25.2.3.0',
+    VERSION: '0.0.25',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
